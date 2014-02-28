02dc787ca3ddff3d15a8067210da36b21ee8ff45
David Pede <david.pede@tasianmedia.com>
2014-02-28 15:21:51 +0000

    Update parameter names

--- core/components/getyoutube/elements/snippets/snippet.getyoutube.php
+++ core/components/getyoutube/elements/snippets/snippet.getyoutube.php
@@ -23,24 +23,24 @@
 
 /* set default properties */
 $id = !empty($id) ? $id : '';
-$outerTpl = !empty($outerTpl) ? $outerTpl : ''; // Blank default makes '&outerTpl' optional
-$rowTpl = !empty($rowTpl) ? $rowTpl : 'videoRowTpl';
-$rowTplOdd = !empty($rowTplOdd) ? $rowTplOdd : 'videoRowTpl';
+$tpl = !empty($tpl) ? $tpl : 'videoRowTpl';
+$tplAlt = !empty($tplAlt) ? $tplAlt : '';
+$tplWrapper = !empty($tplWrapper) ? $tplWrapper : ''; // Blank default makes '&tplWrapper' optional
 $toPlaceholder = !empty($toPlaceholder) ? $toPlaceholder : ''; // Blank default makes '&toPlaceholder' optional
-$i = 1; //Starts row count at 1
+$idx = 0; //Starts index at 0
+
+$output = '';
 
 $feedURL = "https://gdata.youtube.com/feeds/api/videos?q=$id&v=2";
 $videos = simplexml_load_file($feedURL);
 
-$output = '';
-
 if (!empty($id)) {
   foreach($videos->entry as $video) { //'entry' is the xml node for each video
 
-    if($i % 2 == 0){ // Checks if row count can be divided by 2 (even)
-      $tpl = $rowTpl;
+    if($idx % 2 == 0){ // Checks if row count can be divided by 2 (even)
+      $rowTpl = $tpl;
     }else{
-      $tpl = $rowTplOdd;
+      $rowTpl = $tplAlt;
     }
 
     $media = $video->children('http://search.yahoo.com/mrss/'); //Access 'media:group' node
@@ -53,18 +53,18 @@ if (!empty($id)) {
     $modx->setPlaceholder('description',$video->content);
 
     $array = (array) $video; //Convert to an Array
-    $rowOutput .= $modx->getChunk($tpl,$array);
-    $i++; //Increases row count by +1
+    $rowOutput .= $modx->getChunk($rowTpl,$array);
+    $idx++; //Increases row count by +1
   }
   if(!empty($rowOutput)) {
-    if (!empty($outerTpl)) {
+    if (!empty($tplWrapper)) {
       $rows = array('rows' => $rowOutput); //Convert '$rowOutput' to an array and set as 'rows' placeholder
-      $output = $modx->getChunk($outerTpl,$rows); //Pass 'rows' placeholder to the '$outerTpl' chunk
+      $output = $modx->getChunk($tplWrapper,$rows); //Pass 'rows' placeholder to the '$tplWrapper' chunk
     }else{
       $output = $rowOutput;
     }
     if (!empty($toPlaceholder)) {
-      $modx->setPlaceholder($toPlaceholder,$output); //Pass '$outerTpl' chunk to the '$toPlaceholder' placeholder 
+      $modx->setPlaceholder($toPlaceholder,$output); //Pass '$tplWrapper' chunk to the '$toPlaceholder' placeholder 
       return ''; // Stops double output
     }
   }

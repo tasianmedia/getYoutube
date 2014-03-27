3bbea6354ab62d49ccc3678a1de2e1ad566f523d
David Pede <david.pede@tasianmedia.com>
2014-03-27 16:49:30 +0000

    Add 'sortby' parameter

--- core/components/getyoutube/elements/snippets/snippet.getyoutube.php
+++ core/components/getyoutube/elements/snippets/snippet.getyoutube.php
@@ -27,18 +27,19 @@
 $apiKey = $modx->getOption('apiKey',$scriptProperties);
 $channel = !empty($channel) ? "&channelId=" . $channel : '';
 //$id = !empty($id) ? $id : '';
-$pageToken = preg_replace('/[^-a-zA-Z0-9_]/','',$_GET['page']); //For pagination
 $tpl = !empty($tpl) ? $tpl : 'videoRowTpl';
 //$tplAlt = !empty($tplAlt) ? $tplAlt : '';
 //$tplWrapper = !empty($tplWrapper) ? $tplWrapper : ''; //Blank default makes '&tplWrapper' optional
 //$toPlaceholder = !empty($toPlaceholder) ? $toPlaceholder : ''; //Blank default makes '&toPlaceholder' optional
+$sortby = !empty($sortby) ? $sortby : 'date'; //Acceptable values are: date, rating, title, viewCount
+$pageToken = preg_replace('/[^-a-zA-Z0-9_]/','',$_GET['page']); //For pagination
 $idx = 0; //Starts index at 0
 $totalVar = !empty($totalVar) ? $totalVar : 'total';
 $total = 0;
 
 $output = '';
 
-$url = "https://www.googleapis.com/youtube/v3/search?part=id,snippet$channel&type=video&maxResults=40&pageToken=$pageToken&key=$apiKey";
+$url = "https://www.googleapis.com/youtube/v3/search?part=id,snippet$channel&type=video&maxResults=40&order=$sortby&pageToken=$pageToken&key=$apiKey";
 
 $json = file_get_contents($url);
 $videos = json_decode($json, TRUE);
